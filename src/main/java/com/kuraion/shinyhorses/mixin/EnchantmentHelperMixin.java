bab4ad71bb84424c26e98ac241a51084e71a5c4f
Kuraion <n3rilp@gmail.com>
2022-09-11 21:22:22 +0200

    Fixed EnchantmentHelperMixin target

--- src/main/java/com/kuraion/shinyhorses/mixin/EnchantmentHelperMixin.java
+++ src/main/java/com/kuraion/shinyhorses/mixin/EnchantmentHelperMixin.java
@@ -3,16 +3,14 @@ package com.kuraion.shinyhorses.mixin;
 import com.kuraion.shinyhorses.ShinyHorsesMod;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(EnchantmentHelper.class)
+@Mixin(targets = "net.minecraft.world.item.enchantment.EnchantmentHelper")
 public class EnchantmentHelperMixin {
-
-	@Inject(method = "getEnchantmentLevel*",at = @At("HEAD"),cancellable = true)
+	@Inject(method = "getEnchantmentLevel(Lnet/minecraft/world/item/enchantment/Enchantment;Lnet/minecraft/world/entity/LivingEntity;)I",at = @At("HEAD"),cancellable = true)
 	private static void checkHorse(Enchantment enchantmentIn, LivingEntity entityIn, CallbackInfoReturnable<Integer> cir){
 		ShinyHorsesMod.checkHorseHook(enchantmentIn,entityIn,cir);
 	}
