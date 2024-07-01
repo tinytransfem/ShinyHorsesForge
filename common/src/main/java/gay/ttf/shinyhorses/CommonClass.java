07ab1fdf6595a8694bc6929e81ffe836de94c137
Ciri <n3rilp@gmail.com>
2024-07-01 17:20:22 +0200

    Initial 1.21 port

--- common/src/main/java/gay/ttf/shinyhorses/CommonClass.java
+++ common/src/main/java/gay/ttf/shinyhorses/CommonClass.java
@@ -5,6 +5,7 @@ import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.blaze3d.vertex.VertexMultiConsumer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.core.Holder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.animal.horse.Horse;
@@ -40,7 +41,7 @@ public class CommonClass {
         return old;
     }
 
-    public static void checkHorseHook(Enchantment enchantmentIn, LivingEntity entityIn, CallbackInfoReturnable<Integer> cir) {
+    public static void checkHorseHook(Holder<Enchantment> enchantmentIn, LivingEntity entityIn, CallbackInfoReturnable<Integer> cir) {
         if (entityIn instanceof Horse) {
             ItemStack armor = ((Horse) entityIn).getBodyArmorItem();
             if (armor.getItem() instanceof AnimalArmorItem) {
--- common/src/main/java/gay/ttf/shinyhorses/mixin/EnchantmentHelperMixin.java
+++ common/src/main/java/gay/ttf/shinyhorses/mixin/EnchantmentHelperMixin.java
@@ -1,6 +1,7 @@
 package gay.ttf.shinyhorses.mixin;
 
 import gay.ttf.shinyhorses.CommonClass;
+import net.minecraft.core.Holder;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.enchantment.Enchantment;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,8 +12,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(targets = "net.minecraft.world.item.enchantment.EnchantmentHelper")
 public class EnchantmentHelperMixin {
 
-	@Inject(method = "getEnchantmentLevel(Lnet/minecraft/world/item/enchantment/Enchantment;Lnet/minecraft/world/entity/LivingEntity;)I",at = @At("HEAD"),cancellable = true)
-	private static void checkHorse(Enchantment enchantmentIn, LivingEntity entityIn, CallbackInfoReturnable<Integer> cir){
+	@Inject(method = "getEnchantmentLevel(Lnet/minecraft/core/Holder;Lnet/minecraft/world/entity/LivingEntity;)I",at = @At("HEAD"),cancellable = true)
+	private static void checkHorse(Holder<Enchantment> enchantmentIn, LivingEntity entityIn, CallbackInfoReturnable<Integer> cir){
 		CommonClass.checkHorseHook(enchantmentIn,entityIn,cir);
 	}
 }
